ce43c8a3cf83914be1d04144036400ebbb07fdda
jake zahm <jakezahm3@gmail.com>
2026-07-23 20:21:08 -0400

    c2d3feea-9e3a-42db-b517-6e7fed72b503

--- home.nix
+++ home.nix
@@ -111,7 +111,17 @@
   
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+    home.file = {
+    # Generates a physical executable script path on disk for windsurf.nvim
+    ".config/nvim/nix-wrapper.sh" = {
+      executable = true;
+      text = ''
+        #!/bin/sh
+        exec "$@"
+      '';
+    };
+  };
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
